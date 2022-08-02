41b71d17a10527b512ebb14264a9bc58e83ee226
nkjcodes <nirmalkumarjangir.18cs@saividya.ac.in>
2022-08-02 14:05:29 +0530

    adding api call functionality

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ class Hello{
     public void display(){
         System.out.println("This is display method");
     }
+
+    public void apiCall(){
+        System.out.println("Calling API");
+    }
 }
